52c472fc34c36cf0c36f0971747746d0e1305269
Trefu_ <tre.lopez@hotmail.com>
2025-12-02 23:29:55 -0300

    fix typo in input mapping and improve player movement with lerp smoothing and mouse capture toggle

--- efe-pe-manso/src/scripts/TrefuTestPlayer.gd
+++ efe-pe-manso/src/scripts/TrefuTestPlayer.gd
@@ -7,12 +7,29 @@ const jumpVelocity = 4.5
 
 const aim_sensitivity = 0.4
 
+var lerp_speed = 10.0 
+
+var direction = Vector3.ZERO
+
 func _ready():
 	#para que el mouse empiece 'oculto'
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _input(event):
-	if event is InputEventMouseMotion:
+func _unhandled_input(event: InputEvent):
+	
+	# ESC para liberar mouse	
+	if event.is_action_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	# Click para recapturar
+	if event is InputEventMouseButton and event.pressed:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(deg_to_rad(-event.relative.x * aim_sensitivity))
 		%head.rotate_x(deg_to_rad(-event.relative.y * aim_sensitivity))
 		%head.rotation.x = clamp(%head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
@@ -28,8 +45,8 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "foward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	direction =  lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * lerp_speed)
 	if direction:
 		velocity.x = direction.x * currentSpeed
 		velocity.z = direction.z * currentSpeed
