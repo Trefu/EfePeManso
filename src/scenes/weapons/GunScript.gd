b083ba5199b5446b508170b101adb61c2af1fc79
Trefu <tre.lopez@hotmail.com>
2025-12-08 11:38:12 -0300

    feat: add weapon system with shooting mechanics and reorganize asset structure
    - Add "shoot" input action mapped to left mouse button with 0.2 deadzone
    - Import weapon model assets (AK47, AWP, shotgun, MAC10, rocket launchers, grenades) with textures
    - Add bullet projectile models and textures for different weapon types
    - Import muzzle flash texture for weapon effects
    - Add AK47 shooting sound effect
    - Reorganize audio assets: move OstBase.ogg from assets/music to assets/sounds/music
    - Remove unuse

---
+++ src/scenes/weapons/GunScript.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@export var damage = 1.0
+
+@onready var animation: AnimationPlayer = $AnimationPlayer
+@onready var shot_sound: AudioStreamPlayer = $ShotSound
+@onready var raycast: RayCast3D = $RayCast3D
+
+var can_shoot: bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("shoot") and can_shoot and not animation.is_playing():
+		print("disparar")
+		animation.play("shoot")
+		shot_sound.play()
+
+func _on_animation_finished() -> void:
+	can_shoot = true
