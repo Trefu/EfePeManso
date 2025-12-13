7e68db340136de5bb2d24f7fcb355ef623b8525e
Cristian0w0 <cristian.rodriguez.0185@gmail.com>
2025-12-13 19:21:28 -0300

    Push cris primera vez lol

---
+++ src/scenes/player/camera_3d.gd
@@ -0,0 +1,34 @@
+extends Camera3D
+
+const MOUSE_SENSITIVITY: float = 0.06
+var head: Node3D
+var player: CharacterBody3D
+var camera: Camera3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player = get_parent()
+	camera = self
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _unhandled_input(event: InputEvent):
+	# ESC para liberar mouse	
+	if event.is_action_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	# Click para recapturar
+	if event is InputEventMouseButton and event.pressed:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		player.rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSITIVITY))
+		camera.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENSITIVITY))
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
