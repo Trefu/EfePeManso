b002848072e121b205d434269f4513cb8f267af1
Trefu <tre.lopez@hotmail.com>
2025-12-18 01:47:36 -0300

    feat: add 118 black retina crosshair textures
    - Add crosshair001-118.png to Black Retina directory
    - Includes various crosshair styles (dots, circles, crosses, brackets, etc.)
    - All textures in high-resolution retina format

--- src/scenes/player/weapons/Weapon.gd
+++ src/scenes/player/weapons/Weapon.gd
@@ -36,19 +36,20 @@ func _shoot_raycast():
 	var camera = get_viewport().get_camera_3d()
 
 	if not camera:
-		print('camara')
 		return
 	
 	var from = camera.global_position
 	var forward = -camera.global_transform.basis.z
 	
-	var spread_vec = Vector3(
-		randf_range(-weapon_data.spread, weapon_data.spread),
-		randf_range(-weapon_data.spread, weapon_data.spread),
-		0
-	)
+	var spread_x = randf_range(-weapon_data.spread, weapon_data.spread)
+	var spread_y = randf_range(-weapon_data.spread, weapon_data.spread)
 	
-	var to = from + (forward + spread_vec).normalized() * weapon_data.max_range
+	var direction = forward
+	direction.x += spread_x
+	direction.y += spread_y
+	direction = direction.normalized()
+	
+	var to = from + direction * weapon_data.max_range
 	
 	var space = get_world_3d().direct_space_state
 	var query = PhysicsRayQueryParameters3D.create(from, to)
@@ -84,8 +85,28 @@ func _show_muzzle_flash():
 		muzzle_flash_particles.restart()
 		muzzle_flash_particles.emitting = true
 
-func _spawn_hit_effect(_position: Vector3):
-	print('efectos hit')
+#TO DO por ahora esto es provisional y para todas las armas
+func _spawn_hit_effect(hit_position: Vector3):
+	print("Hit en:", hit_position)
+	
+	var debug_sphere = MeshInstance3D.new()
+	debug_sphere.mesh = SphereMesh.new()
+	debug_sphere.mesh.radius = 0.08
+	debug_sphere.mesh.height = 0.16
+	
+	var material = StandardMaterial3D.new()
+	material.albedo_color = Color.RED
+	debug_sphere.material_override = material
+	
+	get_tree().current_scene.add_child(debug_sphere)
+	debug_sphere.global_position = position
+	
+	var tween = create_tween()
+	tween.tween_property(debug_sphere, "scale", Vector3(0.1, 0.1, 0.1), 0.4)
+	tween.parallel().tween_property(material, "albedo_color:a", 0.0, 0.4)
+	
+	await get_tree().create_timer(0.5).timeout
+	debug_sphere.queue_free()
 
 func reload():
 	current_ammo = weapon_data.magazine_size
